d1da3d7a7497797240ad8623f18eb95b0e3396e0
triplicates <triplicatesouls@gmail.com>
2020-09-30 21:59:02 +0400

    [update] Remove not used imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom"
-import {BrowserRouter as Router, Switch, Route} from "react-router"
 import "./components/Index/index.css"
 import Home from "./components/Home/Home.jsx"
 
